a7222c81c88cb58be11b4cca899cb44f875fe521
matias-fdlv <matiasfdlv@gmail.com>
2025-09-01 20:53:48 -0300

    Eliminando descuido

--- www/app/Http/Controllers/Auth/LoginController.php
+++ www/app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
-class LoginController extends Controller
+class LoginController
 {
     public function showLoginForm()
     {
