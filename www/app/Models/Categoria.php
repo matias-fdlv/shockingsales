e71364a09fdd8ff6e28a8f866173b3a748f4e022
matias-fdlv <matiasfdlv@gmail.com>
2025-09-18 21:26:58 -0300

    buguee los commits del dia 18/09/25, ignorar los tres anteriores a este del mismo dia, mas no los proximos.

--- www/app/Models/Categoria.php
+++ www/app/Models/Categoria.php
@@ -6,16 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 //categoria de cada producto.
 class Categoria extends Model
+
 {
+    //tabla a usar
     protected $table = 'categoria';
+
+    //primarykey de la tabla
     protected $primaryKey = 'IDCategoria';
+
+    //fillables
     public $timestamps = false;
+
     protected $fillable = ['Nombre'];
 
+    //belongstomany, a traves de una relacion con la tabla pertenece hacemos un N a N, muchas Categorias pertenecen a muchos ProductosInternos
     public function productos()
-    {
-        // Pivot: pertenece(IDProductoI, IDCategoria)
-        return $this->belongsToMany(ProductoInterno::class, 'pertenece', 'IDCategoria', 'IDProductoI');
-    }
-}
 
+    {
+        return $this->belongsToMany(ProductoInterno::class, 'pertenece', 'IDCategoria', 'IDProductoI');
+
+    }
+
+}
--- www/app/Models/CredencialTienda.php
+++ www/app/Models/CredencialTienda.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 //credencialtienda es donde (supuestamente) guardaremos las token y la informacion necesaria para acceder a la API (???)
 class CredencialTienda extends Model
 {
+    //tabla de la BD
     protected $table = 'credencialesTienda';
+
+    //primarykey de la tabla
     protected $primaryKey = 'IDCredencial';
+
+    //fillables de la tabla
     public $timestamps = false;
     protected $fillable = ['IDTienda', 'Tipo', 'Valor'];
-
+    //esta funcion usa BELONGSTO, registro de credencialtienda (o sea, una token de una tienda) es parte de un registro de la tabla Tienda. no es una cardilanidad tan exacta, simplemente significa que credencial tienda, efectivamente, es de (belongs to) Tienda.
     public function tienda()
     {
         return $this->belongsTo(Tienda::class, 'IDTienda', 'IDTienda');
